237fa7c1bcc9bede9c4cf1e3674275f46744b298
Valentinas Kasteckis <41127259+Tortonas@users.noreply.github.com>
2019-12-30 01:19:09 +0200

    Cookie is set after clicking `remove` that welcome window.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,18 +2,35 @@
 
 namespace App\Controller;
 
+use App\Form\ClosePopupFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="app_home")
+     * @param Request $request
+     * @return Response
      */
-    public function index()
+    public function index(Request $request)
     {
+        $form = $this->createForm(ClosePopupFormType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted())
+        {
+            $cookie = new Cookie('closeWelcomeScreen', 'true', strtotime('now + 10 minutes'));
+            $response = new Response();
+            $response->headers->setCookie($cookie);
+            $response->send();
+        }
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'closePopupForm' => $form->createView(),
         ]);
     }
 }
