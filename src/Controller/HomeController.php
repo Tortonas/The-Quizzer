da559eeac96ce827a16d985a21be69db47867ab6
Valentinas Kasteckis <41127259+Tortonas@users.noreply.github.com>
2020-01-01 17:53:02 +0200

    You can set your nickname without registering to the system

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Question;
 use App\Form\ClosePopupFormType;
+use App\Form\SetCustomNicknameFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,19 +20,53 @@ class HomeController extends AbstractController
      */
     public function index(Request $request)
     {
-        $form = $this->createForm(ClosePopupFormType::class);
-        $form->handleRequest($request);
+        $closePopupForm = $this->createForm(ClosePopupFormType::class);
+        $closePopupForm->handleRequest($request);
 
-        if($form->isSubmitted())
+        if($closePopupForm->isSubmitted())
         {
-            $cookie = new Cookie('closeWelcomeScreen', 'true', strtotime('now + 10 minutes'));
+            $cookie = new Cookie('closeWelcomeScreen', 'true', strtotime('now + 1 year'));
             $response = new Response();
             $response->headers->setCookie($cookie);
             $response->send();
         }
 
+        $customNicknameForm = $this->createForm(SetCustomNicknameFormType::class);
+
+        $customNicknameForm->handleRequest($request);
+
+        if($customNicknameForm->isSubmitted())
+        {
+            if($request->get('username') == null)
+            {
+                $this->addFlash('danger-nickname-form', 'Prašau, nurodykite savo slapyvardį');
+            }
+            else
+            {
+                $cookie = new Cookie('username', $request->get('username'), strtotime('now + 1 year'));
+                $response = new Response();
+                $response->headers->setCookie($cookie);
+                $response->send();
+                return $this->render('/home/nicknameSubmit.html.twig');
+            }
+        }
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $question = $entityManager->getRepository(Question::class)->findBy(array(), array('timeAnswered' => 'ASC'), 1);
+
+        $showQuestion = false;
+
+        if($this->isGranted('ROLE_USER') || $request->cookies->get('username') != null)
+        {
+            $showQuestion = true;
+        }
+
         return $this->render('home/index.html.twig', [
-            'closePopupForm' => $form->createView(),
+            'closePopupForm' => $closePopupForm->createView(),
+            'question' => $question,
+            'setCustomNicknameForm' => $customNicknameForm->createView(),
+            'showQuestion' => $showQuestion,
         ]);
     }
 }
