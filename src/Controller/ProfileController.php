6c8b41187f0b485bdb3c1bde6f01587e2b14c5d3
Valentinas Kasteckis <41127259+Tortonas@users.noreply.github.com>
2020-01-01 21:21:35 +0200

    Clickable buttons change email and change password

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,28 +2,44 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfileController extends AbstractController
 {
     /**
      * @Route("/profilis", name="app_profile")
+     * @IsGranted("ROLE_USER")
      */
     public function index()
     {
         return $this->render('profile/index.html.twig', [
-            'controller_name' => 'ProfileController',
+            'user' => $this->getUser(),
         ]);
     }
 
     /**
      * @Route("/profilis/keisti", name="app_profile_edit")
+     * @IsGranted("ROLE_USER")
+     * @param Request $request
+     * @return Response
      */
-    public function edit()
+    public function edit(Request $request)
     {
-        return $this->render('profile/change.html.twig', [
-            'controller_name' => 'ProfileController',
-        ]);
+        if($request->get('v') == 'p')
+        {
+            return $this->render('profile/changePass.html.twig', [
+                'user' => $this->getUser(),
+            ]);
+        }
+        else if($request->get('v') == 'e')
+        {
+            return $this->render('profile/changeEmail.html.twig', [
+                'user' => $this->getUser(),
+            ]);
+        }
     }
 }
