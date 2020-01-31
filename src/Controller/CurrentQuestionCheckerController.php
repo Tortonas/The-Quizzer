d1c2d9d13ced9daabcab60a265462167047ac408
Valentinas Kasteckis <41127259+Tortonas@users.noreply.github.com>
2020-01-31 17:42:50 +0200

    created API where you can get current question

---
+++ src/Controller/CurrentQuestionCheckerController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Question;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CurrentQuestionCheckerController extends AbstractController
+{
+    /**
+     * @Route("/api/question", name="api_get_current_question")
+     */
+    public function index()
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $currentQuestion = $entityManager->getRepository(Question::class)->findOneBy(array('active' => 1));
+
+        return new Response($currentQuestion->getQuestion());
+    }
+}
