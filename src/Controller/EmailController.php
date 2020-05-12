2476e348284015143690a827b595235098c5d7be
Valentinas Kasteckis <41127259+Tortonas@users.noreply.github.com>
2020-05-12 21:16:33 +0300

    Added symfony mailer and created Email Controller to test messages

---
+++ src/Controller/EmailController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EmailController extends AbstractController
+{
+    /**
+     * @Route("/test_email", name="emails")
+     * @param \Swift_Mailer $mailer
+     * @return Response
+     */
+    public function index(\Swift_Mailer $mailer)
+    {
+        $message = (new \Swift_Message('Hello Email'))
+            ->setFrom('noreply@quizzer.lt')
+            ->setTo('plankton546@gmail.com')
+            ->setBody(
+                $this->renderView(
+                    'emails/registration.html.twig',
+                    ['name' => 'Testuotojas']
+                ),
+                'text/html'
+            );
+
+        $mailer->send($message);
+
+
+        return $this->render('emails/index.html.twig', [
+            'controller_name' => 'EmailController',
+        ]);
+    }
+}
