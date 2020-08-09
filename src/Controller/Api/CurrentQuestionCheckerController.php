ca31878de377f63322c6964a9996f6041e5152c9
Valentinas Kasteckis <plankton546@gmail.com>
2020-08-09 14:43:39 +0300

    Ad question will be updated if no one is playing but someone is viewing this page (for example when streaming and having it as an ad)

--- src/Controller/Api/CurrentQuestionCheckerController.php
+++ src/Controller/Api/CurrentQuestionCheckerController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Api;
 
+use App\Controller\HomeController;
 use App\Entity\Question;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,6 +10,13 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CurrentQuestionCheckerController extends AbstractController
 {
+    private HomeController $homeController;
+
+    public function __construct(HomeController $homeController)
+    {
+        $this->homeController = $homeController;
+    }
+
     /**
      * @Route("/api/question", name="api_get_current_question")
      */
@@ -18,5 +26,27 @@ class CurrentQuestionCheckerController extends AbstractController
         $currentQuestion = $entityManager->getRepository(Question::class)->findOneBy(array('active' => 1));
 
         return new Response($currentQuestion->getQuestion());
+    }
+
+    /**
+     * @Route("/api/reload", name="api_reload_question_if_needed")
+     */
+    public function reloadQuestionIfNeeded()
+    {
+        $currentDateTime = date('Y-m-d H:i:s');
+        $entityManager = $this->getDoctrine()->getManager();
+        $currentQuestion = $entityManager->getRepository(Question::class)->findOneBy(array(
+            'active' => 1));
+
+        // If true, then changing questions to new one.
+        $currentQuestionModifyTime = $currentQuestion->getTimeModified()->getTimestamp()+(60*3);
+        $currentQuestionModifyTime = date("Y-m-d H:i:s", $currentQuestionModifyTime);
+
+        if($currentQuestionModifyTime < $currentDateTime)
+        {
+            $this->homeController->setNewQuestion($entityManager, $currentDateTime, $currentQuestion);
+        }
+
+        return new Response();
     }
 }
