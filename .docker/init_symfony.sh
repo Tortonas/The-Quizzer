f941cd6e0a615b51d358c3ef341a69abaecc7d07
Valentinas Kasteckis <plankton546@gmail.com>
2020-08-12 20:43:55 +0300

    Added phpmyadmin service to docker compose

--- .docker/init_symfony.sh
+++ .docker/init_symfony.sh
@@ -5,5 +5,7 @@ composer install
 php bin/console d:s:u --force
 php bin/console doctrine:fixtures:load --append
 echo "Project has been started"
-echo "Website - http://192.168.2.2/"
-echo "MYSQL - 192.168.2.3:3306"
+echo "Access project website - http://192.168.2.2/"
+echo "MYSQL Server - 192.168.2.3:3306"
+echo "Access phpmyadmin via - http://192.168.2.4/"
+echo "Access server SSH by writing - docker exec -it quizzer_web bash"
