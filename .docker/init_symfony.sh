a6a0cee10c0496724ab775fede099a7aa923f0ef
Valentinas Kasteckis <plankton546@gmail.com>
2020-08-12 19:53:43 +0300

    Added fixtures, added new github action which will run docker-compose, whenever you docker-compose up the project, there will be some data and website will be working

--- .docker/init_symfony.sh
+++ .docker/init_symfony.sh
@@ -3,3 +3,7 @@ sleep 10
 cd /var/www/html
 composer install
 php bin/console d:s:u --force
+php bin/console doctrine:fixtures:load --append
+echo "Project has been started"
+echo "Website - http://192.168.2.2/"
+echo "MYSQL - 192.168.2.3:3306"
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\FreqAskedQuestion;
+use App\Entity\Question;
+use App\Entity\QuestionAnswer;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class AppFixtures extends Fixture
+{
+    private \Doctrine\Persistence\ObjectManager $manager;
+
+    public function load(\Doctrine\Persistence\ObjectManager $manager)
+    {
+        $this->manager = $manager;
+        if ($this->doINeedToUploadFixtures()) {
+            $user = $this->generateUser();
+            $this->generateQuestionsWithAnswers($user);
+            $this->generateFaqs();
+
+            $manager->flush();
+        }
+    }
+
+    private function doINeedToUploadFixtures(): bool
+    {
+        $userCount = $this->manager->getRepository(User::class)->count([]);
+        $questionCount = $this->manager->getRepository(Question::class)->count([]);
+        $questionAnswerCount = $this->manager->getRepository(QuestionAnswer::class)->count([]);
+        $faqCount = $this->manager->getRepository(FreqAskedQuestion::class)->count([]);
+
+        // If true, then I don't need to upload fixtures
+        if ($userCount >= 1 && $questionCount >= 3 && $questionAnswerCount >= 1 && $faqCount  >= 3) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private function generateUser(): User
+    {
+        $user = new User();
+        $user->setEmail('quizzer@quizzer.dev');
+        $user->setEmailSubscription(0);
+        $user->setLastTimeGotEmail(new \DateTime('2020-01-01'));
+        // hashed password of this string -> quizzer
+        $user->setPassword('$argon2id$v=19$m=65536,t=4,p=1$BcBVS+gbaxnApK/kFTEBOw$hZbYuLENZ34yWzvhT05XZHQJSBwkBojLoBoCNgOBp6Y');
+        $user->setUsername('quizzer');
+        $user->setRegisterAt(new \DateTime());
+        $user->setLastVisit(new \DateTime());
+        $user->setRoles([]);
+        $this->manager->persist($user);
+
+        return $user;
+    }
+
+    private function generateQuestionsWithAnswers(User $user): void
+    {
+        $question = new Question();
+        $question->setActive(1);
+        $question->setQuestion('Whats 2+2?');
+        $question->setAnswer('4');
+        $question->setTimeModified(new \DateTime('2020-01-01'));
+        $this->manager->persist($question);
+
+        $question = new Question();
+        $question->setActive(0);
+        $question->setQuestion('Whats 3+3?');
+        $question->setAnswer('6');
+        $question->setTimeModified(new \DateTime('2020-01-02'));
+        $this->manager->persist($question);
+
+        $question = new Question();
+        $question->setActive(0);
+        $question->setQuestion('Whats 5+5?');
+        $question->setAnswer('10');
+        $question->setTimeModified(new \DateTime('2020-01-03'));
+        $this->manager->persist($question);
+
+        $questionAnswer = new QuestionAnswer();
+        $questionAnswer->setUsername('Quizzer');
+        $questionAnswer->setUser($user);
+        $questionAnswer->setQuestion($question);
+        $questionAnswer->setTimeAnswered(new \DateTime());
+        $this->manager->persist($questionAnswer);
+    }
+
+    private function generateFaqs(): void
+    {
+        for ($i = 1; $i <= 3; $i++) {
+            $faq = new FreqAskedQuestion();
+            $faq->setQuestion($i . ' FAQ Demo question');
+            $faq->setAnswer($i . ' FAQ Demo answer');
+            $this->manager->persist($faq);
+        }
+    }
+}
